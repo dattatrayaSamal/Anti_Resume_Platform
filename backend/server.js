4974ad366a4039bc51cc11c8fe96c576e085139a
Nandan Singh Danu <thenandansingh1999@gmail.com>
2025-04-12 01:14:22 +0530

    server file and other files fixed

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-dotenv.config();
-connectDB();
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
+dotenv.config()
+const PORT = process.env.PORT;
+
+
+const userRouter = require("./routes/user.routes")
+const candidateRoutes=require("./routes/candidateRoutes")
+const companyRoutes = require('./routes/companyRoutes')
+const connectDB = require('./config/db');
+
+
 
 // Routes
-app.use('/api/candidate', require('./routes/candidateRoutes'));
-app.use('/api/company', require('./routes/companyRoutes'));
+app.use('/api/candidate',candidateRoutes);
+app.use('/api/company', companyRoutes);
+app.use("/user", userRouter);
+
+
 
 // Error handling
 app.use((err, req, res, next) => {
@@ -18,5 +28,16 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+app.listen(PORT, async() => {
+  try {
+    await connectDB()
+    console.log("connected to db")
+  } catch (error) {
+    console.log("error while connection to db",error)
+  }
+  console.log(`Server started on port ${PORT}`);
+});
